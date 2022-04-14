49134e1205be7aba2e4e0ccc24f6dd9a82168b8e
Julian Mayes <juliantmayes@gmail.com>
2022-04-13 18:22:05 -0700

    sliding selector on click

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = document.getElementById("app");
+const nav = document.getElementById("nav");
+const select = document.getElementById("select");
 
-const createButtons = () => {
+let selected;
+
+//get all cities and create links
+const createLinks = () => {
   fetch("./navigation.json")
     .then((response) => response.json())
     .then((json) => {
@@ -10,15 +15,19 @@ const createButtons = () => {
         link.classList.add("nav-link");
         link.innerHTML = x.label;
         link.addEventListener("click", (e) => {
-          getSelection(e);
+          selectLink(e);
         });
-        app.appendChild(link);
+        nav.appendChild(link);
       });
     });
 };
 
-const getSelection = (e) => {
-  console.log(e.target);
+//on click
+const selectLink = (e) => {
+  select.style.left = `${e.target.offsetLeft}px`;
+  select.style.width = `${e.target.offsetWidth}px`;
+  selected = e.target;
+  console.log(selected);
 };
 
-createButtons();
+createLinks();
