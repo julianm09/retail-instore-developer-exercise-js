9457f28d876d642e657dd3cdbcfbc74f5edc9114
Julian Mayes <juliantmayes@gmail.com>
2022-04-13 20:16:24 -0700

    google api calls for location time

--- index.js
+++ index.js
@@ -1,70 +1,112 @@
 const app = document.getElementById("app");
 const nav = document.getElementById("nav");
-const select = document.getElementById("select");
-
+const indicator = document.getElementById("indicator");
 const clock = document.getElementById("clock");
 
-let selected = null;
+//starting state
+let selected;
+
+window.onload = () => {
+    createLinks();
+}
 
 //get all cities and create links
-const createLinks = () => {
-  fetch("./navigation.json")
+const createLinks = async () => {
+  await fetch("./navigation.json")
     .then((response) => response.json())
     .then((json) => {
-      json.cities.map((x) => {
+      json.cities.map((x, i) => {
+        //create navigation links with id of city and class of nav-link
         const link = document.createElement("div");
         link.id = x.section;
         link.classList.add("nav-link");
         link.innerHTML = x.label;
-        link.addEventListener("click", (e) => {
-          selectLink(e);
-        });
-        link.addEventListener("mouseenter", (e) => {
-          handleMouseEnter(e);
-        });
-        link.addEventListener("mouseout", (e) => {
-          handleMouseOut(e);
-        });
+        link.addEventListener("click", (e) => selectLink(e));
+        link.addEventListener("mouseenter", (e) => handleMouseEnter(e));
+        link.addEventListener("mouseout", (e) => handleMouseOut(e));
         nav.appendChild(link);
+
+        //initialize styling for selected link
+        if (i == 0) {
+          selected = document.getElementById(x.section);
+          selected.style.color = "black";
+          indicator.style.left = `${selected.offsetLeft}px`;
+          indicator.style.width = `${selected.offsetWidth}px`;
+          getLocation();
+          setInterval(updateClock, 1000);
+        }
       });
     });
 };
 
 //on click
 const selectLink = (e) => {
-  select.style.left = `${e.target.offsetLeft}px`;
-  select.style.width = `${e.target.offsetWidth}px`;
+  //move indicator on click
+  indicator.style.left = `${e.target.offsetLeft}px`;
+  indicator.style.width = `${e.target.offsetWidth}px`;
   selected = e.target;
+  document
+    .querySelectorAll(".nav-link")
+    .forEach((x) => (x.style.color = "#8D8D8D"));
   e.target.style.color = "black";
-  console.log(selected);
+
+  getLocation();
+  setInterval(updateClock, 1000);
 };
 
-//on hover
+//on mouse enter
 const handleMouseEnter = (e) => {
   if (selected == null || selected.id !== e.target.id) {
     e.target.style.color = "#3071AA";
   }
 };
 
-//on hover
+//on mouse out
 const handleMouseOut = (e) => {
-  e.target.style.color = "black";
+  if (selected.id == e.target.id) {
+    e.target.style.color = "black";
+  } else {
+    e.target.style.color = "#8D8D8D";
+  }
 };
 
-window.addEventListener('resize', () => {
-    select.style.left = `${selected.offsetLeft}px`;
-    select.style.width = `${selected.offsetWidth}px`;
+window.addEventListener("resize", () => {
+  indicator.style.left = `${selected.offsetLeft}px`;
+  indicator.style.width = `${selected.offsetWidth}px`;
 });
 
 
 
+let timeZone;
+
+const getLocation = async () => {
+  await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${selected.id}&key=AIzaSyBou40yZkrRqVImOAMfhmu-NkPX4sV1jBk`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      const location = json.results[0].geometry.location;
+      getTime(location);
+    });
+};
+
+const getTime = async (location) => {
+  await fetch(
+    `https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat}%2C${location.lng}&timestamp=1331161200&key=AIzaSyBou40yZkrRqVImOAMfhmu-NkPX4sV1jBk`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      timeZone = json.timeZoneId;
+    });
+};
+
 function updateClock() {
-    let today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    clock.innerHTML = time
-  }
-  setInterval(updateClock, 1000);
+  let time = new Date().toLocaleTimeString("en-US", {
+    timeZone: timeZone,
+    timestyle: "full",
+    hourCycle: "h24",
+  });
+  clock.innerHTML = time;
+}
 
-
-
-createLinks();
