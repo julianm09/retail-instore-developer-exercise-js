b8c6755cceafe0f5d144291e9ba20e136d6467d4
Julian Mayes <juliantmayes@gmail.com>
2022-04-13 18:39:59 -0700

    resize slider on window resize

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = document.getElementById("app");
 const nav = document.getElementById("nav");
 const select = document.getElementById("select");
 
-let selected;
+let selected = null;
 
 //get all cities and create links
 const createLinks = () => {
@@ -17,6 +17,12 @@ const createLinks = () => {
         link.addEventListener("click", (e) => {
           selectLink(e);
         });
+        link.addEventListener("mouseenter", (e) => {
+          handleMouseEnter(e);
+        });
+        link.addEventListener("mouseout", (e) => {
+          handleMouseOut(e);
+        });
         nav.appendChild(link);
       });
     });
@@ -27,7 +33,25 @@ const selectLink = (e) => {
   select.style.left = `${e.target.offsetLeft}px`;
   select.style.width = `${e.target.offsetWidth}px`;
   selected = e.target;
+  e.target.style.color = "black";
   console.log(selected);
 };
+
+//on hover
+const handleMouseEnter = (e) => {
+  if (selected == null || selected.id !== e.target.id) {
+    e.target.style.color = "#3071AA";
+  }
+};
+
+//on hover
+const handleMouseOut = (e) => {
+  e.target.style.color = "black";
+};
+
+window.addEventListener('resize', () => {
+    select.style.left = `${selected.offsetLeft}px`;
+    select.style.width = `${selected.offsetWidth}px`;
+});
 
 createLinks();
